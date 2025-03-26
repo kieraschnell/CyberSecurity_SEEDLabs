bf276facdc04455a02662ba18fb41d2c0f194b12
Kiera Schnell <kieraschnell@comcast.net>
2025-03-25 20:01:49 -0500

    kinda a mess

--- Lab3/Labsetup/image_www/Code/defense/unsafe.php
+++ Lab3/Labsetup/image_www/Code/defense/unsafe.php
@@ -1,10 +1,10 @@
 <?php
 // Function to create a sql connection.
 function getDB() {
-  $dbhost = "10.9.0.6";
-  $dbuser = "seed";
-  $dbpass = "dees";
-  $dbname = "sqllab_users";
+  $dbhost="10.9.0.6";
+  $dbuser="seed";
+  $dbpass="dees";
+  $dbname="sqllab_users";
 
   // Create a DB connection
   $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
@@ -15,26 +15,26 @@ function getDB() {
 }
 
 $input_uname = $_GET['username'];
-$input_pwd   = $_GET['Password'];
-$hashed_pwd  = sha1($input_pwd);
+$input_pwd = $_GET['Password'];
+$hashed_pwd = sha1($input_pwd);
 
 // create a connection
 $conn = getDB();
 
-$result = $conn->prepare("SELECT id, name, eid, salary, ssn FROM credential WHERE name = ? AND Password = ?");
-if (!$result) {
-  die("Prepare failed: " . $conn->error);
+// do the query
+$result = $conn->query("SELECT id, name, eid, salary, ssn
+                        FROM credential
+                        WHERE name= '$input_uname' and Password= '$hashed_pwd'");
+if ($result->num_rows > 0) {
+  // only take the first row 
+  $firstrow = $result->fetch_assoc();
+  $id     = $firstrow["id"];
+  $name   = $firstrow["name"];
+  $eid    = $firstrow["eid"];
+  $salary = $firstrow["salary"];
+  $ssn    = $firstrow["ssn"];
 }
 
-$result->bind_param("ss", $input_uname, $hashed_pwd);
-
-$result->execute();
-
-$result->bind_result($id, $name, $eid, $salary, $ssn);
-
-if ($result->fetch()) {
-}
-
-$result->close();
+// close the sql connection
 $conn->close();
 ?>
